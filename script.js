aaa32c38b54701373241cc6b7b852b0b95fd4163
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-25 23:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	class Person{
+		constructor(name , age){
+			this._name = name;
+			this._age = age;
+		}
 
-function Employee(name, age, jobTitle) {}
+		greet(){
+			console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+		}
+	}
+}
+
+function Employee(name, age, jobTitle) {
+	class Employee extends Person{
+		constructor(name, age, jobTitle){
+			this._name = name;
+			this._age = age;
+			this._jobTitle = jobTitle;
+		}
+
+		jobGreet(){
+				console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${jobTitle}.`);
+		}
+	}
+}
+
+const person = new Person("Alice", 25);
+person.greet();
+
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
