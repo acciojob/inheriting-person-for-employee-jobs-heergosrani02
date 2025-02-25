07727fba58cbbe5a40e75d03e3fc7e1a0b853756
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-26 00:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 // complete this js code
-function Person(name, age) {
-	class Person{
-		constructor(name , age){
-			this._name = name;
-			this._age = age;
-		}
+class Person{
+	constructor(name , age){
+		this._name = name;
+		this._age = age;
+	}
 
-		greet(){
-			console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-		}
+	greet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
 	}
 }
 
-function Employee(name, age, jobTitle) {
-	class Employee extends Person{
-		constructor(name, age, jobTitle){
-			this._name = name;
-			this._age = age;
-			this._jobTitle = jobTitle;
-		}
+class Employee extends Person{
+	constructor(name, age, jobTitle){
+		this._name = name;
+		this._age = age;
+		this._jobTitle = jobTitle;
+	}
 
-		jobGreet(){
-				console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${jobTitle}.`);
-		}
+	jobGreet(){
+			console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
 	}
 }
+
 
 const person = new Person("Alice", 25);
 person.greet();
